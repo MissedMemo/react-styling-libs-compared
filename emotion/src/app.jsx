4863aea62666cf18293ae9a0547e68bfd40191f6
AndyL <missedmemo@gmail.com>
2019-02-24 20:02:51 -0800

    exercise styled components syntax version of emotion

--- emotion/src/app.jsx
+++ emotion/src/app.jsx
@@ -1,17 +1,46 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
-import React from 'react'
+import styled from '@emotion/styled'
+import React, { useState } from 'react'
 
 const app = css`
+  display: flex;
   padding: 12px;
-  color: yellow;
   background-color: lightblue;
+`
+
+const textwrapper = css`
+  flex: 1;
+  font-size: 24px;
+  font-style: bold;
+  text-align: center;
+  text-transform: uppercase;
+  align-self: center;
+  color: blue;
 `
 
 // color: yellow; /* var(--error-text-color); */
 
-const App = ({greeting}) => <div css={app}>
-  {greeting}
-</div>
+const ToggleButton = styled.button`
+  font-size: 18px;
+  color: yellow;
+  padding: 8px 20px;
+  margin-right: 5px;
+  background-color: lightgreen;
+`
+
+const App = ({greeting}) => {
+
+  const [ isToggled, setIsToggled ] = useState(false)
+
+  const handleToggle = () => setIsToggled( toggled => !toggled )
+
+  return <div css={app}>
+    <ToggleButton onClick={handleToggle}>
+      { `${ isToggled ? 'Hide' : 'Show'} Greeting` }
+    </ToggleButton>
+    <div css={textwrapper}>{ isToggled && greeting }</div>
+  </div>
+}
 
 export default App
